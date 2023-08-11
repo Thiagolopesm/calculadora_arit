b6bc9619389e3fffb28614fcec71ad6bba3ee939
Thiago Lopes Da Matta <programthiagolopes@gmail.com>
2023-08-11 00:24:25 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Calculadora from './components/Calculadora.vue'
+</script>
+
+<template>
+  <Calculadora />
+</template>
+
+<style>
+  body{
+    background: #2ce75b;
+  }
+</style>
